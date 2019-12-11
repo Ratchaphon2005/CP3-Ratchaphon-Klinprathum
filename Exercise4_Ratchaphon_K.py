4b257da32a4e12f0d32a3abfc9a76e4530122112
Ratchaphon2005 <58630777+Ratchaphon2005@users.noreply.github.com>
2019-12-11 17:06:05 +0700

    Add files via upload

---
+++ Exercise4_Ratchaphon_K.py
@@ -0,0 +1,10 @@
+FE = 80 # Foundation English
+GB = 50 # General Business
+ICS = 60.5 # Introduction to Computer Systems
+CP = 40 # Computer Programming
+print("-------------------Your Score-------------------")
+print("Foundation English :",FE,"Score")
+print("General Business :",GB,"Score")
+print("Introduction to Computer Systems :",ICS,"Score")
+print("Computer Programming :",CP,"Score")
+print("------------------------------------------------")
