11a5ee963caf9b6b9c9eb4cc7324ccd5660b0644
Ratchaphon2005 <58630777+Ratchaphon2005@users.noreply.github.com>
2019-12-30 14:40:51 +0700

    Add files via upload

---
+++ Lecture50_Ratchaphon_K.py
@@ -0,0 +1,14 @@
+x=int(input("Number1 :"))
+y=int(input("Number2 :"))
+def addition(x,y):
+    print(x+y)
+def subtraction(x,y):
+    print(x-y)
+def multiplication(x,y):
+    print(x*y)
+def division(x,y):
+    print(x/y)
+addition(x,y)
+subtraction(x,y)
+multiplication(x,y)
+division(x,y)
